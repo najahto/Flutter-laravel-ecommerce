71cdd30c9b0498b84f646e116f3db1e86429b43b
Taha Omar Najah <tahaomarnajah@gmail.com>
2020-10-19 00:16:30 +0100

    integrated backend template

---
+++ ecom-api/resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.tamplate')
+
+@section('content')
+<div class="container-fluid">
+    <h1 class="dash-title">Blank</h1>
+    <!-- put your rows / columns here -->
+</div>
+@endsection
---
+++ ecom_front/android/app/src/main/kotlin/com/example/ecom_front/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ecom_front
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
